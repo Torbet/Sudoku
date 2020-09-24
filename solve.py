ea7545df855ab7bd2ce4e19f5462c866aa187570
Guy Torbet <guy@guys-mbp.home>
2020-09-24 20:31:15 +0100

    fixed recursion halting

--- solve.py
+++ solve.py
@@ -15,27 +15,34 @@ def refresh():
             print(i, end="   ")
         print('\n')
 
-def possible(y, x, n):
+def possible(x, y, n):
+	for i in range (0, 9):
+		if grid[i][y] == n:
+			return False
+	for j in range (9):
+		if grid[x][j] == n:
+			return False
 
-    if n in grid[y]:
-        return(False)
+	x0 = (x // 3) * 3
+	y0 = (y // 3) * 3
 
-    for i in range(9):
-        if grid[i][x] == n:
-            return(False)
-    return(True)
-
+	for i in range(3):
+		for j in range(3):
+			if grid[x0 + i][y0 + i] == n:
+				return False
+	return True
 
 def solve():
-    for y in range(9):
-        for x in range(9):
-            if grid[y][x] == 0:
-                for n in range(1, 10):
-                    if possible(y, x, n) == True:
-                        grid[y][x] = n
-                        solve()
-    refresh()
+	for y in range(9):
+		for x in range(9):
+			if grid[y][x] == 0:
+				for n in range(1, 10):
+					if possible(y, x, n):
+						grid[y][x] = n
+						solve()
+						grid[y][x] = 0
+				return
+	refresh()
 
 solve()
-    
 
